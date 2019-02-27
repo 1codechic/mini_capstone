1d51144bd99349ab5a627106a018bd26c18f7d53
1codechic <1codechic@devchics.com>
2019-02-26 21:04:35 -0500

    seeded application with product data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+polo2 = Product.new(name: "Polo_red", price: 68, image_url: "poloshirtred@someurl.com", description: "mens red polo shirt")
+polo3 = Product.new(name: "Polo_white", price: 68, image_url: "poloshirtwhite@someurl.com", description: "mens white polo shirt")
+pant1 = Product.new(name: "Polo_jeans", price: 87, image_url: "polojeans@someurl.com", description: "mens polo jeans")
+pant2 = Product.new(name: "denim_white", price: 87, image_url: "denimwhite@someurl.com", description: "mens white jeans")
+pant3 = Product.new(name: "denim_black", price: 87, image_url: "denimblack@someurl.com", description: "demim black jeans")
+
+polo2.save
+polo3.save
+pant1.save
+pant2.save
+pant3.save
