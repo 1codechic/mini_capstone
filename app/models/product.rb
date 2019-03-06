7d7b1cee4f8b954fafcfb68519efc074942b8e85
1codechic <1codechic@devchics.com>
2019-03-05 21:40:33 -0500

    added model methods for is_discounted?, tax, total. Added to show view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price < 10
+    else
+      false
+    end
+  end
+
+  def tax
+   tax_rate = price * 0.09
+  end
+
+   def total
+   tax_rate = price * 0.09
+   total_price = price + tax_rate
+  end
 end
